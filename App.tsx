3aa839e113d067af2880abd84b05b4d1d54c22a9
luanws <luan.w.silveira@gmail.com>
2020-06-30 20:08:27 -0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,35 +1,35 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import {} from './src/firebase-config';
+import { } from './src/firebase-config';
 import { StatusBar } from 'react-native';
 import { AppLoading } from 'expo';
 import {
-  Inter_400Regular,
-  Inter_700Bold,
-  Inter_900Black,
-  useFonts,
+    Inter_400Regular,
+    Inter_700Bold,
+    Inter_900Black,
+    useFonts,
 } from '@expo-google-fonts/inter';
 import colors from './src/res/colors';
 import Routes from './src/routes';
 
 const App: React.FC = () => {
-  const [fontsLoaded] = useFonts({
-    Inter_400Regular,
-    Inter_700Bold,
-    Inter_900Black,
-  });
+    const [fontsLoaded] = useFonts({
+        Inter_400Regular,
+        Inter_700Bold,
+        Inter_900Black,
+    });
 
-  if (!fontsLoaded) {
-    return <AppLoading />;
-  }
+    if (!fontsLoaded) {
+        return <AppLoading />;
+    }
 
-  return (
-    <NavigationContainer>
-      <StatusBar backgroundColor={colors.primary} />
-      <Routes />
-    </NavigationContainer>
-  );
+    return (
+        <NavigationContainer>
+            <StatusBar backgroundColor={colors.primary} />
+            <Routes />
+        </NavigationContainer>
+    );
 };
 
 export default App;
