e522cfcedc1566ac799fa507185234503c64a4b2
Luan Silveira <46425069+SilveiraL@users.noreply.github.com>
2020-06-30 01:52:01 -0300

    configurações de navegação

---
+++ src/res/colors.ts
@@ -0,0 +1,24 @@
+const colors = {
+    primary: '#0275d8',
+    primaryLight: '#5fa3ff',
+    primaryDark: '#004aa6',
+
+    dark: '#282a36',
+    background: '#fafafa',
+    transparent: '#ffffff00',
+
+    success: '#5cb85c',
+    successLight: '#8feb8b',
+    successDark: '#26872f',
+    info: '#5bc0de',
+    infoLight: '#91f3ff',
+    infoDark: '#1490ac',
+    warning: '#f0ad4e',
+    warningLight: '#ffdf7d',
+    warningDark: '#ba7e1d',
+    danger: '#d9534f',
+    dangerLight: '#ff847b',
+    dangerDark: '#a21e27',
+}
+
+export default colors
---
+++ src/routes.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+import colors from './res/colors'
+
+
+import Home from './pages/Home/Home'
+
+const Stack = createStackNavigator()
+
+const Routes = () => {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator screenOptions={{
+                headerTintColor: 'white',
+                headerStyle: {
+                    backgroundColor: colors.primary
+                },
+                cardStyle: {
+                    backgroundColor: colors.background
+                }
+            }}>
+                <Stack.Screen name="Home" component={Home} options={{ title: "Início" }} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    )
+}
+
+export default Routes
