393f7cda80b6cfca075674092224db422d7cfeb3
luanws <luan.w.silveira@gmail.com>
2020-07-02 23:25:31 -0300

    botão do carrinho de compras menu web

--- src/components/Menu/MenuWeb/index.tsx
+++ src/components/Menu/MenuWeb/index.tsx
@@ -1,24 +1,42 @@
 import React, { useRef } from 'react'
 import { View, Text, TouchableOpacity } from 'react-native'
 import { useAuth } from '../../../hooks/auth'
+import { Feather } from '@expo/vector-icons'
 
 import styles from './styles'
+import { useNavigation } from '@react-navigation/native'
+
+interface Props {
+    onPress: () => void
+}
 
 export default () => {
     const { signOut } = useAuth()
 
-    const MenuItem = () => (
+    const navigation = useNavigation()
+
+    const navigateToCart = () => navigation.navigate('Cart') 
+
+    const MenuItem: React.FC<Props> = (props) => (
         <TouchableOpacity
-            onPress={() => signOut()}
+            onPress={props.onPress}
             style={styles.menuItem}
-        >
-            <Text style={styles.menuItemText}>Sair</Text>
-        </TouchableOpacity>
+        >{props.children}</TouchableOpacity>
     )
+
+    const MenuItemText: React.FC<Props> = (props) => (
+        <MenuItem onPress={props.onPress}>
+            <Text style={styles.menuItemText}>Sair</Text>
+        </MenuItem>
+    )
+
 
     return (
         <View style={styles.container}>
-            <MenuItem />
+            <MenuItem onPress={navigateToCart}>
+                <Feather name="shopping-cart" size={24} color="white" />
+            </MenuItem>
+            <MenuItemText onPress={signOut} />
         </View>
     )
 }
