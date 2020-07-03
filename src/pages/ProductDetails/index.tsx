09eb02d2c8054b0ed6cf834eec5bb50e14cbdec9
luanws <luan.w.silveira@gmail.com>
2020-07-03 04:03:48 -0300

    Tela de detalhes do produto

--- src/pages/ProductDetails/index.tsx
+++ src/pages/ProductDetails/index.tsx
@@ -1,19 +1,60 @@
-import React, { useRef, useState, useEffect } from 'react'
-import { View, Text, ScrollView } from 'react-native'
+import React from 'react'
+import { View, Text, ScrollView, Image } from 'react-native'
 import { useRoute } from '@react-navigation/native'
+import { Octicons, MaterialIcons, AntDesign, FontAwesome5 } from '@expo/vector-icons'
 
-interface Params {
+import Product from '../../models/product'
+import colors from '../../res/colors'
+import styles from './styles'
+import { TouchableOpacity } from 'react-native-gesture-handler'
 
-}
 
 const ProductDetails = () => {
     const route = useRoute()
-    const routeParams = route as Params
+    const product = new Product(route.params as Product)
+
+    const Available = () => (
+        <>
+            {product.available ?
+                <View style={styles.row}>
+                    <Octicons name="verified" size={24} color="green" />
+                    <Text style={styles.textItemIcon}>Produto disponível</Text>
+                </View> :
+                <View style={styles.row}>
+                    <MaterialIcons name="do-not-disturb" size={24} color="red" />
+                    <Text style={styles.textItemIcon}>Produto indisponível</Text>
+                </View>
+            }
+        </>
+    )
 
     return (
-        <View>
-            <Text>Detalhes do produto</Text>
-        </View>
+        <ScrollView>
+            <TouchableOpacity style={styles.addToCart}>
+                <FontAwesome5 name="cart-plus" size={24} color="green" />
+            </TouchableOpacity>
+            <Image source={{ uri: product.imageUrl }} style={styles.image} />
+            <View style={styles.container}>
+                <View style={styles.rowSpaceBetween}>
+                    <Text style={styles.name}>{product.name}</Text>
+                    <Text style={styles.price}>{product.getPriceMoneyFormat()}</Text>
+                </View>
+
+                <Available />
+                <View style={styles.row}>
+                    <AntDesign name="tag" size={24} color="orange" />
+                    <Text style={styles.textItemIcon}>Corredor: {product.hall}</Text>
+                </View>
+                <View style={styles.row}>
+                    <AntDesign name="tag" size={24} color="orange" />
+                    <Text style={styles.textItemIcon}>Categoria: {product.category}</Text>
+                </View>
+                <View style={styles.row}>
+                    <AntDesign name="tag" size={24} color="orange" />
+                    <Text style={styles.textItemIcon}>Marca: {product.brand}</Text>
+                </View>
+            </View>
+        </ScrollView>
     )
 }
 
--- src/pages/ProductDetails/styles.ts
+++ src/pages/ProductDetails/styles.ts
@@ -1,6 +1,46 @@
 import { StyleSheet } from 'react-native'
+import colors from '../../res/colors'
 
 const styles = StyleSheet.create({
+    image: {
+        height: 128,
+        borderRadius: 5,
+        resizeMode: 'contain',
+        padding: 16,
+    },
+    addToCart: {
+        alignItems: 'flex-end',
+        margin: 16,
+    },
+    row: {
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        alignItems: 'center',
+        marginVertical: 8,
+        top: 0,
+        right: 0
+    },
+    rowSpaceBetween: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        flexWrap: 'wrap',
+    },
+    container: {
+        marginVertical: 8,
+        marginHorizontal: 16,
+    },
+    name: {
+        fontWeight: 'bold',
+        fontSize: 18
+    },
+    price: {
+        fontWeight: 'bold',
+        fontSize: 20,
+        color: '#070'
+    },
+    textItemIcon: {
+        marginLeft: 8
+    }
 })
 
 export default styles
