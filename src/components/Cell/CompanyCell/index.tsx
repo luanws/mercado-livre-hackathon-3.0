743c2b8fc66b6f96c4820c57c8c3f4c1be3b7f05
Luan Silveira <46425069+SilveiraL@users.noreply.github.com>
2020-07-05 02:50:24 -0300

    célula de empresa

---
+++ src/components/Cell/CompanyCell/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { View, Text, Image } from 'react-native'
+import Company from '../../../models/company'
+
+import styles from './styles'
+
+interface Props {
+    company: Company
+    onPress?: (company: Company) => void
+}
+
+const CompanyCell: React.FC<Props> = (props) => {
+    const company = new Company(props.company)
+    return (
+        <View style={styles.container}>
+            <Image style={styles.image} source={{ uri: company.logoUrl }} />
+            <View>
+                <Text>{company.name}</Text>
+                <Text>{company.corporateName}</Text>
+                <Text>{company.city} - {company.uf}</Text>
+            </View>
+        </View>
+    )
+}
+
+export default CompanyCell
---
+++ src/components/Cell/CompanyCell/styles.ts
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native'
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    image: {
+        width: 64,
+        height: 64,
+        marginRight: 16,
+    }
+})
+
+export default styles
