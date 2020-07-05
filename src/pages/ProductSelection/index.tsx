e4fc4c676aa10538f7dcc2d0c0181ee260f42d79
luanws <luan.w.silveira@gmail.com>
2020-07-04 22:25:31 -0300

    pesquisa por produtos

--- src/pages/ProductSelection/index.tsx
+++ src/pages/ProductSelection/index.tsx
@@ -1,10 +1,11 @@
-import React, { useRef, useState, useEffect } from 'react'
-import { View, Text, ScrollView } from 'react-native'
+import React, { useRef, useState, useEffect, useCallback } from 'react'
+import { View, Text, ScrollView, TextInput } from 'react-native'
 
-import { useAuth } from '../../hooks/auth'
-import { useNavigation } from '@react-navigation/native'
 import * as firebase from 'firebase'
 
+import { Form } from '@unform/mobile'
+import Input from '../../components/Input'
+import { FormHandles } from '@unform/core'
 import ListProductsByHall from '../../components/List/ListProductsByHall'
 import Product from '../../models/product'
 
@@ -12,8 +13,26 @@ import styles from './styles'
 
 const db = firebase.database()
 
+interface FormSearch {
+    search: string
+}
+
 const ProductSelection: React.FC = () => {
-    const [products, setProducts] = useState<Product[]>([])
+    const [filteredProductsAvailable, setFilteredProductsAvailable] = useState<Product[]>([])
+    const [productsAvailable, setProductsAvailable] = useState<Product[]>([])
+    const [search, setSearch] = useState<string>('')
+    const searchFormRef = useRef<FormHandles>(null)
+
+    function handleSubmitSearch(data: FormSearch) {
+        setSearch(data.search)
+    }
+
+    useEffect(() => {
+        const filteredProductsAvailable = productsAvailable.filter(
+            p => p.name.toLowerCase().includes(search.toLowerCase())
+        )
+        setFilteredProductsAvailable(filteredProductsAvailable)
+    }, [productsAvailable, search])
 
     useEffect(() => {
         const listener = db.ref('products')
@@ -30,8 +49,8 @@ const ProductSelection: React.FC = () => {
                 })
 
                 products.sort((a, b) => a.name.localeCompare(b.name))
-                const availableProducts = products.filter(p => p.available === true)
-                setProducts(availableProducts)
+                const productsAvailable = products.filter(p => p.available === true)
+                setProductsAvailable(productsAvailable)
             })
 
         return () => listener.off()
@@ -39,7 +58,22 @@ const ProductSelection: React.FC = () => {
 
     return (
         <ScrollView>
-            <ListProductsByHall products={products} />
+            <Form
+                ref={searchFormRef}
+                style={styles.formSearch}
+                onSubmit={handleSubmitSearch}
+            >
+                <Input
+                    autoCorrect={false}
+                    autoCapitalize="sentences"
+                    name="search"
+                    icon="search"
+                    placeholder="Pesquisar produtos..."
+                    returnKeyType="search"
+                    onSubmitEditing={() => searchFormRef.current?.submitForm()}
+                />
+            </Form>
+            <ListProductsByHall products={filteredProductsAvailable} />
         </ScrollView>
     )
 }
--- src/pages/ProductSelection/styles.ts
+++ src/pages/ProductSelection/styles.ts
@@ -1,6 +1,11 @@
 import { StyleSheet } from 'react-native'
 
+import colors from '../../res/colors'
+
 const styles = StyleSheet.create({
+    formSearch: {
+        margin: 16
+    }
 })
 
 export default styles
